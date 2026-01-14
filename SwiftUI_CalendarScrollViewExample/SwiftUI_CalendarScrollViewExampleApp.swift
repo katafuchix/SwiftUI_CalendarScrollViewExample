15035ef60a7a292d7f8c118658494d0801daf1f9
katafuchix <katafuchix@gmail.com>
2026-01-14 21:49:41 +0900

    Initial Commit

---
+++ SwiftUI_CalendarScrollViewExample/SwiftUI_CalendarScrollViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalendarScrollViewExampleApp.swift
+//  SwiftUI_CalendarScrollViewExample
+//
+//  Created by cano on 2026/01/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalendarScrollViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
